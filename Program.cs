812e4b20850846233262e96283e86b884a472a36
ILLIBOY <illionairedan@ssem.re.kr>
2016-02-09 22:02:42 +0900

    byte는 0~255 sbyte는 -128~127 까지 둘 다 255를 넣고, 다르게 출력되는 것이다. 오

--- Program.cs
+++ Program.cs
@@ -8,25 +8,11 @@ namespace int_example
 	{
 		public static void Main (string[] args)
 		{
-			sbyte a = -500000000000;
-			byte b = 40;
+			byte a = 255;
+			sbyte b = (sbyte)a;
 
-			Console.WriteLine ("a={0}, b={1}", a, b);
-
-			short c = -30000;
-			ushort d = 60000;
-
-			Console.WriteLine ("c={0}, d={1}", c, d);
-
-			int e = -10000000;
-			uint f = 300000000;
-
-			Console.WriteLine ("e={0}, f={1}", e, f);
-
-			long g = -500000000000;
-			ulong h = 2000000000000000000;
-
-			Console.WriteLine ("g={0}, h={1}", g, h);
+			Console.WriteLine (a);
+			Console.WriteLine (b);
 		}
 	}
 }
